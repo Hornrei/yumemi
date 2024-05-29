cf433c18b0225d5851652d36d5297125d7e56e55
hornrei <hornrei0808@gmail.com>
2024-05-30 01:20:30 +0900

    地方ごとに分ける機能追加

--- kadai/src/App.js
+++ kadai/src/App.js
@@ -11,14 +11,28 @@ import {
   Legend,
   ResponsiveContainer,
 } from 'recharts'
+import './App.css'
 
 const apiKey = process.env.REACT_APP_API_KEY
+
+const regions = {
+  '北海道地方': ['北海道'],
+  '東北地方': ['青森県', '岩手県', '宮城県', '秋田県', '山形県', '福島県'],
+  '関東地方': ['茨城県', '栃木県', '群馬県', '埼玉県', '千葉県', '東京都', '神奈川県'],
+  '中部地方': ['新潟県', '富山県', '石川県', '福井県', '山梨県', '長野県', '岐阜県', '静岡県', '愛知県'],
+  '近畿地方': ['三重県', '滋賀県', '京都府', '大阪府', '兵庫県', '奈良県', '和歌山県'],
+  '中国地方': ['鳥取県', '島根県', '岡山県', '広島県', '山口県'],
+  '四国地方': ['徳島県', '香川県', '愛媛県', '高知県'],
+  '九州地方': ['福岡県', '佐賀県', '長崎県', '熊本県', '大分県', '宮崎県', '鹿児島県', '沖縄県'],
+}
+
 
 export default function App() {
   const [prefectures, setPrefectures] = useState([])
   const [populationData, setPopulationData] = useState([])
   const [selectedPrefectures, setSelectedPrefectures] = useState([])
   const [ageGroup, setAgeGroup] = useState('総人口')
+  const [loading, setLoading] = useState(true)
 
   // 47都道府県を配列に格納
   const allPrefectures = prefectures.map((prefecture) => prefecture.prefName)
@@ -41,7 +55,6 @@ export default function App() {
 
   // 人口構成データの取得
   useEffect(() => {
-
     if (selectedPrefectures.length === 0) return
     
     //選択したagegroupの値を取得
@@ -84,8 +97,7 @@ export default function App() {
     }
 
     fetchPopulationData()
-  }, [selectedPrefectures, ageGroup ])
-
+  }, [selectedPrefectures, ageGroup, loading ])
 
 
 
@@ -111,6 +123,15 @@ export default function App() {
     })
     return acc
   }, [])
+  
+  // 起動時に1回だけ実行
+  useEffect(() => {
+    if (!loading) return
+    console.log("初回だけ")
+    setLoading(false)
+  }, [loading])
+
+
 
   return (
     <>
@@ -118,26 +139,37 @@ export default function App() {
         <h1 className="header__title">都道府県別の総人口推移グラフ</h1>
       </header>
       <div className="wrapper">
+
         <h2>都道府県</h2>
-        <div className="prefecture__container">
-          {prefectures.map((prefecture) => (
-            <div key={prefecture.prefCode} className="prefecture__item">
-              <input
-                className="prefecture__input"
-                type="checkbox"
-                id={`checkbox-${prefecture.prefCode}`}
-                name={prefecture.prefName}
-                onChange={() => handleCheckbox(prefecture.prefCode)}
-              />
-              <label
-                className="prefecture__label"
-                htmlFor={`checkbox-${prefecture.prefCode}`}
-              >
-                {prefecture.prefName}
-              </label>
+        <div className="tihou_container"> 
+          {Object.keys(regions).map((region) => (
+            <div key={region} className='tihou'>
+              <h3>{region}</h3>
+              <div className="prefecture__container">
+                {prefectures
+                  .filter((pref) => regions[region].includes(pref.prefName))
+                  .map((prefecture) => (
+                    <div key={prefecture.prefCode} className="prefecture__item">
+                      <input
+                        className="prefecture__input"
+                        type="checkbox"
+                        id={`checkbox-${prefecture.prefCode}`}
+                        name={prefecture.prefName}
+                        onChange={() => handleCheckbox(prefecture.prefCode)}
+                      />
+                      <label
+                        className="prefecture__label"
+                        htmlFor={`checkbox-${prefecture.prefCode}`}
+                      >
+                        {prefecture.prefName}
+                      </label>
+                    </div>
+                  ))}
+              </div>
             </div>
+          
           ))}
-      </div>
+        </div> {/* end tihou_container */}
 
         <h2>年齢層選択</h2>
         <div>
@@ -185,7 +217,10 @@ export default function App() {
             <LineChart data={formattedData}>
               <CartesianGrid strokeDasharray="3 3" />
               <XAxis dataKey="year" />
-              <YAxis />
+              <YAxis 
+              // カンマを付ける
+              tickFormatter={value => value.toLocaleString()}
+              />
               <Tooltip />
               <Legend />
               {selectedPrefectures.map((prefCode) => {
